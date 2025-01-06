48538a64da2d66685b633ecd5a7189b91ef6ad09
llddang <bodyness@naver.com>
2025-01-06 17:12:08 +0900

    🎉 add : quiz03. 홀짝 판별기

--- quiz3.js
+++ quiz3.js
@@ -1,6 +1,6 @@
-function evenOrOdd(num) {
+function numToStrEvenOrOdd(num) {
   return num % 2 ? "홀수" : "짝수";
 }
 
-console.log(evenOrOdd(10)); // 결과값 "짝수";
-console.log(evenOrOdd(7)); // 결과값 "홀수";
+console.log(numToStrEvenOrOdd(10)); // 결과값 "짝수";
+console.log(numToStrEvenOrOdd(7)); // 결과값 "홀수";
