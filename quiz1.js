fadeed6e7cccdaff4b760c17a75c1cd469763f9c
llddang <bodyness@naver.com>
2025-01-06 17:11:43 +0900

    🎉 add : quiz01. 빈칸 채우기 문제

--- quiz1.js
+++ quiz1.js
@@ -2,11 +2,11 @@
 let uninitialized;
 console.log(uninitialized); // undefined
 
-// 2. 재할당할 때 에러가 나는 것은 const입니다.
+// 2. 재할당할 때 에러가 나는 변수 선언자는 const입니다.
 const apple = "사과";
 apple = "바나나"; // TypeError: Assignment to constant variable
 
-// 3. 배열은 0부터 시작하니 3은 4번째 요소인 19가 출력됩니다.
+// 3. 배열은 0부터 시작함으로 3은 4번째 요소인 19가 출력됩니다.
 let lotto = [3, 8, 13, 19, 21, 32];
 console.log(lotto[3]); // 19
 
