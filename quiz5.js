41bc0dd40bfe03a0de31fdbec6e2616e2aef465b
llddang <bodyness@naver.com>
2025-01-06 17:12:24 +0900

    🎉 add : quiz05. 점수 수정하기

--- quiz5.js
+++ quiz5.js
@@ -1,7 +1,7 @@
 const scores = [36, 62, 72, 55, 86, 95, 92, 48, 81];
 
 function editScores(scores) {
-  for(let i=0; i<scores.length; i++) scores[i] = calc(scores[i], "+", 3);
+  for(let i=0; i<scores.length; i++) scores[i] = calculator(scores[i], "+", 3);
 }
 
 editScores(scores);
@@ -9,10 +9,7 @@ editScores(scores);
 console.log(scores);
 // 기대값: [39, 65, 75, 58, 89, 98, 95, 51, 84]
 
-
-
-
-function calc(a, op, b) {
+function calculator(a, op, b) {
   switch(op){
     case "+": return a + b;
     case "-": return a - b;
